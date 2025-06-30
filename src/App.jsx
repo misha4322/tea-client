fe250f04036d21637681d95f3bcdb2bad48e5bdd
karasi001 <mishnefnara@gmail.com>
2025-07-01 01:15:44 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,44 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Provider, useDispatch } from "react-redux";
-import store from "./app/store";
-import { initializeUser } from "./features/userSlice";
-
-import { Root } from "./routes/Root/Root";
-import { ErrorPage } from "./routes/ErorPage/ErrorPage";
-import { Home } from "./routes/Home/Home";
-import { ConfComputer } from "./routes/ConfComputer/CofComputer";
-import { ProductBasket } from "./routes/ProductBasket/ProductBasket";
-import Login from "./routes/Auth/Login";
-import Register from "./routes/Auth/Register";
-import Profile from "./routes/Profile/Profile";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { Navigate } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from './routes/Root/Root.jsx'; 
+import ErrorPage404 from './routes/ErrorPage404/ErrorPage404.jsx';
+import HomePage from "./routes/Home/Home.jsx";
+import Cart from "./routes/Cart/Cart.jsx";
+import Profile from "./routes/Profile/Profile.jsx";
+import Register from "./routes/Register/Register.jsx";
+import Login from "./routes/Login/Login.jsx";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
-    errorElement: <ErrorPage />,
+    errorElement: <ErrorPage404 />,
     children: [
-      { index: true, element: <Home /> },
-      { path: "login", element: <Login /> },
-      { path: "register", element: <Register /> },
       {
-        path: "gather",
-        element: (
-          <ProtectedRoute>
-            <ConfComputer />
-          </ProtectedRoute>
-        ),
+        index: true,
+        element: <HomePage />,
       },
       {
-        path: "basket",
-        element: (
-          <ProtectedRoute>
-            <ProductBasket />
-          </ProtectedRoute>
-        ),
+        path: 'cart', 
+        element: <Cart />,
       },
       {
-        path: "profile",
-        element: (
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        ),
+        path: 'profile',
+        element: <Profile />,
       },
-      { path: "*", element: <Navigate to="/" replace /> },
-    ],
-  },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+    ]
+  }
 ]);
 
-const AppInitializer = () => {
-  const dispatch = useDispatch();
-  
-  React.useEffect(() => {
-    dispatch(initializeUser());
-  }, [dispatch]);
-
+function App() {
   return <RouterProvider router={router} />;
-};
-
-function AppWrapper() {
-  return (
-    <Provider store={store}>
-      <AppInitializer />
-    </Provider>
-  );
 }
 
-export default AppWrapper;
+export default App;
